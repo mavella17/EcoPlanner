f28ceab8975ce8f5fd679f720644b7a694355c17
Adejola Ogunsan <adejolaogunsan@gmail.com>
2023-07-20 14:00:41 +0000

    Adding WTForms and Jinja Styling to templates

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import git
 from flask import Flask, jsonify, render_template
 from flask import url_for, flash, redirect, request
+from forms import driveData, flightData
 import pandas as pd
 import sqlalchemy as db
 from sqlalchemy import select
 from sqlalchemy.sql import text as sa_text
 app = Flask(__name__)
-
-
+proxied = FlaskBehindProxy(app)
+app.config['SECRET_KEY'] = 'c275b91d07ca2bdd6359'
 @app.route("/")
 @app.route("/home")
 def home():
@@ -25,6 +26,20 @@ def webhook():
         return 'Updated PythonAnywhere successfully', 200
     else:
         return 'Wrong event type', 400
+
+@app.route("/drive", methods=['GET', 'POST'])
+def drive_data():
+    form = driveData()
+    if form.validate_on_submit():
+        return redirect(url_for('results'))
+    return render_template('drive.html', form=form)
+
+@app.route('/flights', methods=['GET', 'POST'])
+def flight_data():
+    form = flightData()
+    if form.validate_on_submit():
+        return redirect(url_for('results'))
+    return render_template('flights.html', form=form)
 
 
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, SelectField, RadioField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, InputRequired
+
+#create a class to encapsulate data from the drive form 
+class driveData(FlaskForm):
+   #TODO: need to change the vehiclemodel paramter to match what is required by the API
+   vehiclemodel = StringField('Vehicle Model', validators=[InputRequired()])
+   #TODO: add a feature where distance can be change from miles to kilometers
+   distance = IntegerField('Distance', validators=[DataRequired()])
+   distance_type = SelectField('Miles or Kilometers', choices=[('mi', 'Miles'), ('km', 'Kilometers')])
+   submit = SubmitField('Calculate Emissions')
+   
+#create a class to encapsulate data from the flights form
+class flightData(FlaskForm):
+    wherefrom = StringField('Where From?', validators=[DataRequired(), Length(min=1, max=20)])
+    whereto = StringField('Where To?', validators=[DataRequired(), Length(min=1, max=20)])
+    submit = SubmitField('Calculate Emissions')

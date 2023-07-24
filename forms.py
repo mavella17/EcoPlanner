654853621fa9aec62a74ca2b7b9f19f216ae0980
Adejola Ogunsan <adejolaogunsan@gmail.com>
2023-07-24 19:39:26 +0000

    Fixing Validation

--- forms.py
+++ forms.py
@@ -2,14 +2,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms import BooleanField, IntegerField, SelectField, RadioField
 from wtforms.validators import DataRequired, Length, Email
-from wtforms.validators import EqualTo, InputRequired
+from wtforms.validators import EqualTo, InputRequired, NumberRange
 
 
 # create a class to encapsulate data from the drive form
 class driveData(FlaskForm):
     vehiclemodel = StringField('Vehicle Model', validators=[InputRequired()])
     # TODO: add a feature where distance can be change from miles to kilometers
-    distance = IntegerField('Distance', validators=[DataRequired()])
+    distance = IntegerField('Distance', validators=[DataRequired(), NumberRange(min=0, max=10)])
     distance_type = SelectField('Miles or Kilometers',
                                 choices=[('mi', 'Miles'),
                                          ('km', 'Kilometers')])
@@ -19,9 +19,9 @@ class driveData(FlaskForm):
 # create a class to encapsulate data from the flights form
 class flightData(FlaskForm):
     wherefrom = StringField('Where From?',
-                            validators=[DataRequired(), Length(min=1, max=20)])
+                            validators=[DataRequired(), Length(min=1, max=4)])
     whereto = StringField('Where To?',
-                          validators=[DataRequired(), Length(min=1, max=20)])
+                          validators=[DataRequired(), Length(min=1, max=4)])
     passengers = IntegerField("Passengers:",
                           validators=[DataRequired()])
     submit = SubmitField('Calculate Emissions')
@@ -33,3 +33,4 @@ class registrationData(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField(validators=[DataRequired()])
     submit = SubmitField('Sign Up!')
+    
